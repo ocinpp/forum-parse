b26f196db6bfebb20ae609a0dd9e7667d57a78c3
ocinpp <ocin25@gmail.com>
2016-05-17 01:52:49 +0800

    new fields in topic

--- src/main/java/test/ForumTopic.java
+++ src/main/java/test/ForumTopic.java
@@ -5,8 +5,50 @@ import java.util.List;
 
 public class ForumTopic {
 
+	private String author;
+	private String topicDate;
+	private Integer replyCount;
+	private Integer viewCount;
+	private String lastPost;
+	private String url;
 	private List<ForumPost> posts;
 	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTopicDate() {
+		return topicDate;
+	}
+	public void setTopicDate(String topicDate) {
+		this.topicDate = topicDate;
+	}
+	public Integer getReplyCount() {
+		return replyCount;
+	}
+	public void setReplyCount(Integer replyCount) {
+		this.replyCount = replyCount;
+	}
+	public Integer getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(Integer viewCount) {
+		this.viewCount = viewCount;
+	}
+	public String getLastPost() {
+		return lastPost;
+	}
+	public void setLastPost(String lastPost) {
+		this.lastPost = lastPost;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
 	public List<ForumPost> getPosts() {
 		return Collections.unmodifiableList(posts);
 	}
